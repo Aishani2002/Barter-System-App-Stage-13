5605fa4fd3f52da81753f3d0099a067e5cab956f
Aishani2002 <aishani.acharya@gmail.com>
2020-10-20 13:20:06 +0530

    stage-11-12

--- screens/ItemRequestScreen.js
+++ screens/ItemRequestScreen.js
@@ -17,29 +17,113 @@ export default class ItemRequestScreen extends Component{
     this.state ={
       userId : firebase.auth().currentUser.email,
       itemName:"",
-      reasonToTrade:""
+      reasonToRequest:"",
+      isItemRequestActive:"",
+      requestedItemName:"",
+      itemStatus:"",
+      requestId:"",
+      userDocId:"",
+      docId:"",
     }
   }
+
+  getItemRequest=()=>{
+var itemRequest= db.collection("requested_items").where("user_id","==",this.state.userId).get()
+snapshot.forEach((doc)=>{
+ if(doc.data().item_status==="requested"){
+   this.setState({
+     requestId:doc.data().request_id,
+     requestedItemName:doc.data().item_name,
+     itemStatus:doc.data().item_status,
+     docId:doc.id
+   })
+ }
+})
+  }
+
+  isItemRequestActive () {
+db.collection("users").where("email_id","==",this.state.userId)
+onSnapshot((snapshot) => {
+snapshot.forEach((doc)=>{
+  this.setState({
+    isItemRequestActive:doc.data().isItemRequestActive,
+    userDocId:doc.id
+  })
+})
+})
+  }
+
+  recievedItem=(itemName) => {
+    var userId = this.state.userId
+    var requestId = this.state.requestId
+    db.collection("recieved_items").add({
+      "user_id": userId,
+      "item_name": itemName,
+      "request_id": requestId,
+      "item_status":"recieved"
+    })
+  }
+
+  sendNotification=()=>{
+    
+  db.collection("users").where("request_id","==",this.state.requestId).get()
+  .then((snapshot)=>{
+    snapshot.forEach((doc)=>{
+      var donorId= doc.data().donor_id
+      var itemName= doc.data().item_name
+    })
+    db.collection("all_notifications").add({
+      "targeted_user_id":donorId,
+      "message":name+" "+lastName+" recieved the item"+itemName,
+      notification_status:"unread",
+      item_name:itemName
+    })
+  })
+  }
+
+  updateItemRequestStatus=()=>{
+    db.collection("requested_items").doc(this.state.docId).update({
+      "item_status":"recieved",
+    })
+    db.collection("users").where("email_id","==",this.state.userId).get()
+  .then((snapshot)=>{
+    snapshot.forEach((doc)=>{
+      db.collection("users").doc(doc.id).update({
+        isItemRequestActive:false
+      })
+    
+    })
+  })
+}
+
 
   createUniqueId(){
     return Math.random().toString(36).substring(7);
   }
 
-
-
-  addRequest =(itemName,reasonToTrade)=>{
+  addRequest = async (itemName,reasonToRequest)=>{
     var userId = this.state.userId
     var randomRequestId = this.createUniqueId()
     db.collection('requested_items').add({
         "user_id": userId,
         "item_name":itemName,
-        "reason_to_trade":reasonToTrade,
+        "reason_to_request":reasonToRequest,
         "request_id"  : randomRequestId,
+        "item_status":"requested",
+        "date":firebase.firestore.FieldValue.serverTimestamp(),
     })
-
+    await this.getItemRequest()
+    db.collection("users").where("email_id","==",userId).get()
+    .then((snapshot) =>{
+      snapshot.forEach((doc)=>{
+        db.collection('users').doc(doc.id).update({
+          isItemRequestActive:true
+        })
+      })
+    })
     this.setState({
         itemName :'',
-        reasonToTrade : ''
+        reasonToRequest : ''
     })
 
     return Alert.alert("Item Requested Successfully")
@@ -47,6 +131,33 @@ export default class ItemRequestScreen extends Component{
 
 
   render(){
+    if(this.state.isItemRequestActive===true){
+      return(
+        <View style={{flex:1,justifyContent:"center"}}>
+          <View style={{borderColor:"orange",borderWidth:2,justifyContent:"center",alignItems:'center',padding:10,margin:10}}>
+          <Text> Item Name</Text>
+      <Text>{this.state.requestedItemName}</Text>
+          </View>
+          <View style={{borderColor:"orange",borderWidth:2,justifyContent:"center",alignItems:'center',padding:10,margin:10}}>
+          <Text> Item Status</Text>
+      <Text>{this.state.itemStatus}</Text>
+          </View>
+          <TouchableOpacity style={{borderColor:"orange",borderWidth:1,backgroundColor:"orange",width:300,alignSelf:"center",alignItems:'center',height:30,marginTop:30}}
+          
+          onPress={()=>{
+            this.sendNotification()
+            this.updateItemRequestStatus()
+              this.recievedItem(this.state.requestedItemName)
+
+          }}
+          >
+            <Text>I recieved the item</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+
+    else{
     return(
         <View style={{flex:1}}>
           <MyHeader title="Request Item" navigation ={this.props.navigation}/>
@@ -65,17 +176,17 @@ export default class ItemRequestScreen extends Component{
                 style ={[styles.formTextInput,{height:300}]}
                 multiline
                 numberOfLines ={8}
-                placeholder={"Why do you need the item"}
+                placeholder={"Why do you need the Item"}
                 onChangeText ={(text)=>{
                     this.setState({
-                        reasonToTrade:text
+                        reasonToRequest:text
                     })
                 }}
-                value ={this.state.reasonToReque}
+                value ={this.state.reasonToRequest}
               />
               <TouchableOpacity
                 style={styles.button}
-                onPress={()=>{this.addRequest(this.state.itemName,this.state.reasonToTrade)}}
+                onPress={()=>{this.addRequest(this.state.itemName,this.state.reasonToRequest)}}
                 >
                 <Text>Request</Text>
               </TouchableOpacity>
@@ -83,6 +194,7 @@ export default class ItemRequestScreen extends Component{
         </View>
     )
   }
+}
 }
 
 const styles = StyleSheet.create({
